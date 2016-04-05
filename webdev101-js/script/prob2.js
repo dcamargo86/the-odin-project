f6b5994ad6888752d3994c129374af3f10a12c72
dcamargo86 <dcamargo86@gmail.com>
2016-04-05 11:22:56 -0300

    Update prob2.js

--- webdev101-js/script/prob2.js
+++ webdev101-js/script/prob2.js
@@ -1,23 +1,24 @@
-var fib = function(max) {
+var fib = function() {
 
-  var f = [1, 2]; // defining the first two elements 
-  var x = 2; // for some reason, the sum of the evens is not considering 2 on the var f, so i declared x beginning with this value.
-
-  // despite i prefer using while, i couldn´t figure out a way to make it work, so i came with this sort of sloppy solution, where the max var will be the highest fibonacci number lower than 4.000.000 (found by brute force)
+  var f = [1,1,2];
+  var i = 2;
+  var x = 0;
   
-  for (var i = 2; i < max; i++) {
-
+  while (f[i] <= 4000000) {
+  
     f[i] = f[i - 1] + f[i - 2];
-
+    f.push(f[i]);
+    i++;
+    
     if (f[i] % 2 === 0) {
-
+      
       x += f[i];
-
     }
+
   }
 
   return x;
 
 }
 
-console.log(fib(32));
+console.log(fib());
